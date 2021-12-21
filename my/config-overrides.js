2fbc0c58faa4029a9c374af18e780c902145a9d6
saya_zhang <saya_zhang@jiliguala.com>
2021-12-21 15:37:42 +0800

    feat: redux_mobx

--- my/config-overrides.js
+++ my/config-overrides.js
@@ -1,15 +1,35 @@
+const {
+  override,
+  addDecoratorsLegacy,
+  addWebpackAlias,
+  fixBabelImports,
+  addLessLoader,
+} = require("customize-cra")
 
-import { override, addDecoratorsLegacy } from 'customize-cra';
-
-const path = require('path');
+const path = require("path");
 
 module.exports = override(
+  fixBabelImports("import", {
+    libraryName: "antd",
+    libraryDirectory: "es",
+    style: true,
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: {
+      "@primary-color": "#1BC7DB"
+    },
+    cssLoaderOptions: {},
+  }),
+  addWebpackAlias({
+    "@": path.resolve(__dirname, "src"),
+  }),
   addDecoratorsLegacy(),
   (config) => {
     const devtoolMap = {
-      development: 'cheap-module-source-map'
+      development: "cheap-module-source-map",
     };
     config.devtool = devtoolMap[config.mode] || false;
     return config;
   }
-)
+);
